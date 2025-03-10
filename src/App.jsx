3785526fec1b732a40269bbd7dbce51a9172d3a6
ragayathri97 <ragayathri97@gmail.com>
2025-03-10 16:32:56 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/auth" element={<AuthForm />} />
-            <Route path="/product/:id" element={<ProductDetail />} /> {/* New route */}
+            <Route path="/product/:id" element={<ProductDetail />} />
           </Routes>
         </main>
         <Footer />
