339ed1a3fe4c5b70d138c00326ac9aeee74e6352
ragayathri97 <ragayathri97@gmail.com>
2025-03-10 18:41:17 +0530

    changes

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-// app.use("/ECommerceHub", express.static(path.join(__dirname, "dist")));
-// app.get("/ECommerceHub/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "dist", "index.html"));
-// });
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use("/ECommerceHub", express.static(path.join(__dirname, "dist")));
+app.get("/ECommerceHub/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
